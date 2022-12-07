28b8848e90e26fc60d284e76aa31217ae5b3b456
alexsony <alexsony1997@gmail.com>
2022-12-07 16:36:34 +0200

    Implemented functionality for multiple pipes.

--- main.c
+++ main.c
@@ -78,6 +78,7 @@ int getch() {
 }
 
 int processInput(char command[], char *** split, const char *delim) {
+    printf("%s", command);
     char *tmp_str = command;
     int count = 0; 
 
@@ -133,6 +134,7 @@ int executeCommand(char command[]) {
 //file descriptor is unique number acrros process  =>handle of the input/output resource
 int executePipes(char left_command[], char right_command[]) {
     int fd[2]; //file descriptor fd[0] -> read end of pipe; fd[1] -> write end of pipe
+    int status_code;
     if (pipe(fd) == -1) {
         return 1;
     }
@@ -150,8 +152,13 @@ int executePipes(char left_command[], char right_command[]) {
 
         char **left;
         processInput(left_command, &left, " ");
-        execvp(left[0], left);
+        status_code = execvp(left[0], left);
         free(left);
+
+        if (-1 == status_code) {
+            printf("\n%s: command not found!", left_command);
+            exit(1);
+        }
     }
 
     int pid2 = fork();
@@ -167,8 +174,13 @@ int executePipes(char left_command[], char right_command[]) {
 
         char **right;
         processInput(right_command, &right, " ");
-        execvp(right[0], right);
+        status_code = execvp(right[0], right);
         free(right);
+
+        if (-1 == status_code) {
+            printf("\n%s: command not found!", right_command);
+            exit(1);
+        }
     }
 
     close(fd[0]);
@@ -181,12 +193,96 @@ int executePipes(char left_command[], char right_command[]) {
 
 }
 
+int multiPipes(char **command, int no_pipes) {
+    
+    int **fd = (int **)malloc(no_pipes * sizeof(int*));
+    for(int i = 0; i < no_pipes; i++) fd[i] = (int *)malloc(2 * sizeof(int));
+    int pid, status_code;
+
+    for (int i = 0; i < no_pipes; ++i) {
+        if (pipe(fd[i]) == -1) {
+            exit(1);
+        }
+    }
+
+    if ((pid = fork()) == -1) {
+        exit(1);
+    } else if (pid == 0) {
+        dup2(fd[0][1], STDOUT_FILENO);
+
+        close(fd[0][0]);
+        close(fd[0][1]);
+
+        char **one;
+        processInput(command[0], &one, " ");
+        status_code = execvp(one[0], one);
+
+        free(one);
+
+        if (-1 == status_code) {
+            printf("\n%s: command not found!", command[0]);
+            exit(1);
+        }
+    }
+
+    for (int i = 0; i < no_pipes - 1; ++i) {
+        if ((pid = fork()) == -1) {
+            exit(1);
+        } else if (pid == 0) {
+            dup2(fd[i][0], STDIN_FILENO);
+            dup2(fd[i + 1][1], STDOUT_FILENO);
+
+            close(fd[i][0]);
+            close(fd[i][1]);
+            close(fd[i + 1][0]);
+            close(fd[i + 1][1]);
+
+            char **cmd;
+            processInput(command[i + 1], &cmd, " ");
+            status_code = execvp(cmd[0], cmd);
+
+            free(cmd);
+
+            if (-1 == status_code) {
+                printf("\n%s: command not found!", command[i + 1]);
+                exit(1);
+            }
+        }
+        close(fd[i][0]);
+        close(fd[i][1]);
+    }
+
+    if ((pid = fork()) == -1) {
+        exit(1);
+    } else if (pid == 0) {
+        dup2(fd[no_pipes - 1][0], STDIN_FILENO);
+
+        close(fd[no_pipes - 1][0]);
+        close(fd[no_pipes - 1][1]);
+
+        char **three;
+        processInput(command[no_pipes], &three, " ");
+        status_code = execvp(three[0], three);
+
+        free(three);
+
+        if (-1 == status_code) {
+            printf("\n%s: command not found!", command[no_pipes]);
+            exit(1);
+        }
+    }
+
+    return 0;
+
+}
+
 void runCommand(char command[]) {
     char **split_command;
     int no_pipes = processInput(command, &split_command, "|");
 
     if (0 == no_pipes) executeCommand(split_command[no_pipes]);
-    else executePipes(split_command[0], split_command[1]);
+    else if(1 == no_pipes) executePipes(split_command[0], split_command[1]);
+    // else multiPipes(split_command, no_pipes);
 
     free(split_command);
 }
@@ -229,11 +325,12 @@ int runTerminal() {
 
         case KEY_ENTER:
             command_length = strlen(command); 
-            if(command_length) runCommand(command);
+            if(command_length) {
+                history[history_index] = (char *)malloc(sizeof(char) * command_length);
+                strcpy(history[history_index], command);
+                runCommand(command);
+            }
 
-            history[history_index] = (char *)malloc(sizeof(char) * command_length);
-
-            strcpy(history[history_index], command);
             memset(command, 0, command_length);
             // printf("\nHistory : %s\n", history[history_index]);
             printf("\n");
@@ -266,7 +363,19 @@ int runTerminal() {
 
 int main() {
 
+    char command1[] = "ps aux";
+    char command2[] = "grep root";
+    char command3[] = "grep sbin";
+    char **split_command;
+
+    char command[] = "ps aux | grep root | grep sbin | gredqp usr";
+    int pipes = processInput(command, &split_command, "|");
+
     runTerminal();
+    // multiPipes(split_command, pipes);
+    // printf("%s", command[0]);
+    // test1(command1, command2, command3);
+
     // printLogo();
     return 0;
 }
