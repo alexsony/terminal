138bf19d35a3a43e51c67a311fbe201004864b2e
alexsony <alexsony1997@gmail.com>
2022-12-13 20:15:18 +0200

    Print errors on incorrect commands.

--- src/Process_Manager.c
+++ src/Process_Manager.c
@@ -16,7 +16,6 @@ int msleep(long msec)
 
     if (msec < 0) return -1;
     
-
     ts.tv_sec = msec / 1000;
     ts.tv_nsec = (msec % 1000) * 1000000;
 
@@ -44,7 +43,7 @@ void setWrite(int* rpipe)
 int forkAndChain(int* lpipe, int* rpipe, char command[])
 {
     int pid = fork();
-    int status;
+    int status_code;
 
     if (pid < 0) {
         return 1;
@@ -58,18 +57,21 @@ int forkAndChain(int* lpipe, int* rpipe, char command[])
         if(rpipe) // there's a pipe to the next process
             setWrite(rpipe);
         // else you may want to redirect out to somewhere else for the end
-        // sleep(1);
         char **cmd;
         processInput(command, &cmd, " ");
-        int status_code = execvp(cmd[0], cmd);
-        free(cmd);
-
-        if (-1 == status_code) {
-            printf("\n%s: command not found!", command);
-            _exit(EXIT_FAILURE);
+        int check_type_command = handleCustomCommand(cmd[0], cmd);
+        if (check_type_command) {
+            status_code = execvp(cmd[0], cmd);
+            free(cmd);
+            if (-1 == status_code) {
+                fprintf(stderr,"\r\n%s: command not found!", command);
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            exit(EXIT_SUCCESS);
         }
-        exit(EXIT_SUCCESS);
     }
+    return 0;
 }
 
 int executeCommand(char command[]) {
@@ -86,8 +88,8 @@ int executeCommand(char command[]) {
             int status_code = execvp(cmd[0], cmd);
             free(cmd);
             if (-1 == status_code) {
-                printf("\n%s: command not found!", command);
-                exit(1);
+                fprintf(stderr,"\r\n%s: command not found!", command);
+                exit(EXIT_FAILURE);
             }
         } else {
             exit(EXIT_SUCCESS);
@@ -97,7 +99,6 @@ int executeCommand(char command[]) {
     return 0;
 }
 
-
 int executePipes(char **command, int no_pipes) {
 
     int lpipe[2], rpipe[2];
@@ -106,16 +107,17 @@ int executePipes(char **command, int no_pipes) {
     pipe(rpipe);  
 
     // first child takes input from somewhere else
-    forkAndChain(NULL, rpipe, command[0]);  
+    forkAndChain(NULL, rpipe, command[0]);
+    // printf("\n%s: command not found!", command[0]);
     // output pipe becomes input for the next process.
     lpipe[0] = rpipe[0];
     lpipe[1] = rpipe[1];
 
     // chain all but the first and last children
-    for(int i = 1; i < no_pipes - 1; i++)
+    for(int i = 1; i < no_pipes; ++i)
     {
         pipe(rpipe); // make the next output pipe
-        forkAndChain(lpipe, rpipe, command[i]);
+        forkAndChain(lpipe, rpipe, command[i]); 
         close(lpipe[0]); // both ends are attached, close them on parent
         close(lpipe[1]);
         lpipe[0] = rpipe[0]; // output pipe becomes input pipe
